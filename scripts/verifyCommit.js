41825b2af3243a5cceaf3cc1fcc2103f46f022b1
Marvin Rudolph <info@marvin-rudolph.de>
2020-05-02 15:06:05 +0200

    chore: use husky, add lslint config

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,7 +1,7 @@
 // Invoked on the commit-msg git hook by yorkie.
 
 const chalk = require('chalk');
-const msgPath = process.env.GIT_PARAMS;
+const msgPath = process.env.HUSKY_GIT_PARAMS;
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 
 const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
