27c73d68faec43d3c614975c581dc5a370b86ec7
Marvin Rudolph <info@marvin-rudolph.de>
2020-05-03 19:47:53 +0200

    fix(vue-renderer): correct export name

--- packages/vue-renderer/src/components/index.ts
+++ packages/vue-renderer/src/components/index.ts
@@ -2,7 +2,7 @@
 export { default as RichTextRenderer } from './RichTextRenderer.vue';
 
 // Fallback component
-export { default as SbFallback } from './ComponentFallback.vue';
+export { default as SbComponentFallback } from './ComponentFallback.vue';
 
 // Block components
 export * from './blocks';
